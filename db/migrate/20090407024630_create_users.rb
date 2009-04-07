a4a9997867b543be1263f34c445eba423b975383
Tobias Ford <toby.ford@pobox.com>
2009-04-06 22:49:50 -0400

    added some good stuff to the user model like it's columns

--- db/migrate/20090407024630_create_users.rb
+++ db/migrate/20090407024630_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
+      t.column :screen_name,  :string
+      t.column :email,        :string
+      t.column :password,     :string
       t.timestamps
     end
   end
