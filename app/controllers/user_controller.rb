10255a9c93baac10a621f0911233a2410f85fcba
Tobias Ford <toby.ford@pobox.com>
2009-04-07 21:25:14 -0400

    added the initial register method.. added some debug_dump stuff

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,15 @@ class UserController < ApplicationController
 
   def register
     @title = "Register"
+    if request.post?
+      # Output goes to log file (log/development.log in development mode)
+      logger.info params[:user].inspect
+      
+      @user = User.new(params[:user])
+      if @user.save
+        render :text => "User created!"
+      end
+    end
   end
 
 end
