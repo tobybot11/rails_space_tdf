6dcefa36c601c992b8b984e6e61a40dc8e23daba
Tobias Ford <toby.ford@pobox.com>
2009-04-12 14:50:54 -0400

    adding a way to login and a tweak to the debug part of css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,23 @@ class UserController < ApplicationController
     @title = "RailsSpace User Hub"
   end
 
+  def login
+    @title = "Log in to Railspace"
+    if request.post? and params[:user]
+      @user = User.new(params[:user])
+      user = User.find_by_screen_name_and_password(@user.screen_name, @user.password)
+      if user
+        session[:user_id] = user.id
+        flash[:notice] = "User #{user.screen_name} logged in!"
+        redirect_to :action => 'index'
+      else
+        # Don't show the password in the view.
+        @user.password = nil
+        flash[:notice] = "Invalid screen name/password combination"
+      end
+    end
+  end
+  
   def register
     @title = "Register"
     if request.post? and params[:user]
